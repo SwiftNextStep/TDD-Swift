65c20af02a0667d2396b5d63a1d1ada30cc5cab9
Icaro <icarolavrador@gmail.com>
2016-02-07 10:34:00 +1300

    Initial Commit

---
+++ division/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  division
+//
+//  Created by Icaro Barreira Lavrador on 7/02/16.
+//  Copyright © 2016 Swift Next Step. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
